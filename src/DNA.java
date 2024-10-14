4a56b28af3fba9d9f6c3c3a2ad932452ff48741d
npersily1 <noah.persily@menloschool.org>
2024-10-14 09:57:24 -0700

    Worklog

--- src/DNA.java
+++ src/DNA.java
@@ -4,10 +4,10 @@
  * A puzzle created by Zach Blick
  * for Adventures in Algorithms
  * at Menlo School in Atherton, CA
- *</p>
+ * </p>
  * <p>
  * Completed by: [Noah]
- *</p>
+ * </p>
  */
 
 public class DNA {
@@ -15,36 +15,47 @@ public class DNA {
     public static int STRCount(String sequence, String STR) {
 
 
-        int[] map = new int[22];
-        map['a'-'a'] = 0b00000000;
-        map['c'-'a'] = 0b00000001;
-        map['g'-'a'] = 0b00000010;
-        map['t'-'a'] = 0b00000011;
+        int[] map = new int[128];
+        map['A'] = 0b00000000;
+        map['C'] = 0b00000001;
+        map['G'] = 0b00000010;
+        map['T'] = 0b00000011;
 
         int length = STR.length();
 
-        int temp = 0;
+        int str = 0;
 
         for (int i = 0; i < length; i++) {
-            temp = temp | map[STR.charAt(i) - 'a'];
+            str = str | map[STR.charAt(i)];
+            str = str << 2;
+        }
+        int temp = 0;
+        for (int i = 0; i < length; i++) {
+            temp = temp | map[STR.charAt(i)];
             temp = temp << 2;
         }
+        int maxCount = 0;
+        int count = 0;
+        if (temp == str) {
+            count += 1;
+            maxCount += 1;
+        }
 
-        for (int i = 0; i < sequence.length(); i++) {
-
+        for (int i = length; i < sequence.length(); i++) {
+            temp = temp << 2;
+            temp = temp | map[sequence.charAt(i)];
+            if(temp == str) {
+                maxCount = Math.max(maxCount, ++count);
+                temp = temp << (length - 2);
+            }
+            else {
+                count = 0;
+            }
         }
 
 
-
-
-        return 0;
+        return maxCount;
     }
-
-
-
-
-
-
 
 
 }
