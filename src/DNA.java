ce07c23c5c9e22eeec21a8c06d78e9a5afaa430c
npersily1 <noah.persily@menloschool.org>
2024-10-14 20:20:15 -0700

    Worklog

--- src/DNA.java
+++ src/DNA.java
@@ -16,23 +16,24 @@ public class DNA {
 
 
         int[] map = new int[128];
-        map['A'] = 0b00000000;
-        map['C'] = 0b00000001;
-        map['G'] = 0b00000010;
-        map['T'] = 0b00000011;
+        map['A'] = 0;
+        map['C'] = 1;
+        map['G'] = 2;
+        map['T'] = 3;
 
         int length = STR.length();
 
         int str = 0;
 
         for (int i = 0; i < length; i++) {
-            str = str | map[STR.charAt(i)];
             str = str << 2;
+            str = str | map[STR.charAt(i)];
         }
         int temp = 0;
         for (int i = 0; i < length; i++) {
-            temp = temp | map[STR.charAt(i)];
             temp = temp << 2;
+            temp = temp | map[sequence.charAt(i)];
+
         }
         int maxCount = 0;
         int count = 0;
@@ -40,13 +41,15 @@ public class DNA {
             count += 1;
             maxCount += 1;
         }
+        String s;
 
         for (int i = length; i < sequence.length(); i++) {
-            temp = temp << 2;
+
             temp = temp | map[sequence.charAt(i)];
             if(temp == str) {
                 maxCount = Math.max(maxCount, ++count);
-                temp = temp << (length - 2);
+                temp = 0;
+                i+=length;
             }
             else {
                 count = 0;
