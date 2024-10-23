21d79b4799c41addb7843f8162794e81aab32e0b
npersily1 <noah.persily@menloschool.org>
2024-10-22 19:55:00 -0700

    Worklog

--- src/DNA.java
+++ src/DNA.java
@@ -14,47 +14,48 @@ public class DNA {
 
     public static int STRCount(String sequence, String STR) {
 
-
+        // Quick lookup for character values  STR length
         int[] map = new int[128];
         map['A'] = 0;
         map['C'] = 1;
         map['G'] = 2;
         map['T'] = 3;
+        final int RADIX = 4;
 
         int length = STR.length();
 
+        // Horners method to find the hash value of the STR and the first possible sequence
         int str = 0;
 
-
         for (int i = 0; i < length; i++) {
-            str = (str * 4 + map[STR.charAt(i)]);
+            str = (str * RADIX + map[STR.charAt(i)]);
         }
-
 
         int seqHash = 0;
+
         for (int i = 0; i < length; i++) {
-            seqHash = (seqHash * 4 + map[sequence.charAt(i)]);
+            seqHash = (seqHash * RADIX + map[sequence.charAt(i)]);
 
         }
-        // Compare first
+        // Compare the first sequence to STR
         int maxCount = 0;
         int count = 0;
         if (seqHash == str) {
             count += 1;
             maxCount += 1;
         }
-
+        // For every letter in the sequence starting where the first possible STR is
         for (int i = length; i < sequence.length(); i++) {
-            seqHash = singleHash(seqHash, map[sequence.charAt(i-length)],map[sequence.charAt(i)],length);
-            String test = sequence.substring(i-length,i);
-            if(seqHash == str) {
+            // Get its hash value
+            seqHash = singleHash(seqHash, map[sequence.charAt(i - length)], map[sequence.charAt(i)], length);
+            String test = sequence.substring(i - length, i);
+            // Reigning champ to keep track of count
+            if (seqHash == str) {
                 count++;
-                if(count > maxCount) {
+                if (count > maxCount) {
                     maxCount = count;
                 }
-            }
-            else
-            {
+            } else {
                 count = 0;
             }
 
@@ -63,10 +64,13 @@ public class DNA {
 
         return maxCount;
     }
-    public static int singleHash(int seqHash,int lastletter, int newLetter, int length)
-    {
-        seqHash -= (lastletter << ((length - 1) * 2));
-        seqHash = seqHash << 2;
+
+    // Method to return new hash if the box is moved
+    // Subtract the value of the last letter, shift it by 2 bits, and add the new letter.
+    public static int singleHash(int seqHash, int lastletter, int newLetter, int length) {
+//        seqHash -= (lastletter << ((length - 1) * 2));
+        seqHash = seqHash << (32 - (2 * length));
+        seqHash = seqHash >> (32 - (2 * length)) - 2;
         seqHash += newLetter;
 
         return seqHash;
