8746bb55922ced831789c83204cd9d97b35ee717
npersily1 <noah.persily@menloschool.org>
2024-10-24 10:29:34 -0700

    Worklog

--- src/DNA.java
+++ src/DNA.java
@@ -15,7 +15,7 @@ public class DNA {
     public static int STRCount(String sequence, String STR) {
 
 
-        // Quick lookup for character values  STR length
+        // Quick lookup for character values in the STR and the length of the STR
         int[] map = new int[128];
         map['A'] = 0;
         map['C'] = 1;
@@ -24,8 +24,9 @@ public class DNA {
 
 
         int length = STR.length();
-
-        final int transferConstant = (1 << (length * 2 - 2)) - 1;
+        int sequenceLength = sequence.length();
+        // Bit mask for rabin
+        final int BIT_MASK = (1 << (length * 2 - 2)) - 1;
 
         // Horners method to find the hash value of the STR and the first possible sequence
         int str = 0;
@@ -40,31 +41,40 @@ public class DNA {
             seqHash = (seqHash << 2) + map[sequence.charAt(i)];
 
         }
+        
+        
 
         int maxCount = 0;
         int count = 0;
 
         // For every letter in the sequence starting where the first possible STR is
-        for (int i = length; i < sequence.length(); i++) {
-
+        for (int i = length; i < sequenceLength; i++) {
 
             // Reigning champ to keep track of count
-              if (seqHash == str) {
-                maxCount= Math.max(++count,maxCount);
-
+            if (seqHash == str) {
+                maxCount = Math.max(++count, maxCount);
+                
+                // Make a new hash via horners method and increment i so when the loop updates i length places will have been skipped
                 seqHash = 0;
                 i += length - 1;
+                if(i > sequenceLength) {
+                    return maxCount;
+                }
                 for (int j = length; j > 0; j--) {
-                    char c = sequence.charAt(i-j+1);
-                     seqHash = (seqHash << 2) + map[sequence.charAt(i - j+1)];
+                    seqHash = (seqHash << 2) + map[sequence.charAt(i - j + 1)];
                 }
 
 
-
             } else {
+                // If it is not a match set streak to zero and excecute Rabin Karp
                 count = 0;
-                seqHash = singleHash(seqHash, map[sequence.charAt(i)], transferConstant);
+                seqHash = singleHash(seqHash, map[sequence.charAt(i)], BIT_MASK);
             }
+        }
+        
+        // Check if the last sequence is a str and update if neccesary
+        if(seqHash == str) {
+            maxCount = Math.max(++count, maxCount);
         }
 
 
@@ -73,8 +83,8 @@ public class DNA {
 
     // Method to return new hash if the box is moved
     // Subtract the value of the last letter, shift it by 2 bits, and add the new letter.
-    public static int singleHash(int seqHash, int newLetter, int transferConstant) {
-        seqHash = seqHash & transferConstant;
+    public static int singleHash(int seqHash, int newLetter, int BIT_MASK) {
+        seqHash = seqHash & BIT_MASK;
         seqHash = seqHash << 2;
         seqHash += newLetter;
 
